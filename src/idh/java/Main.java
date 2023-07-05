769bd5ae237c3affa814f89ffa1f7e9048ce4358
Nils Reiter <nils.reiter@uni-koeln.de>
2023-07-05 13:04:48 +0200

    initial commit

---
+++ src/idh/java/Main.java
@@ -0,0 +1,13 @@
+package idh.java;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt");
+	}
+
+}
